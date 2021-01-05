505abe33e1904385251d7584c0748e075edbb6ef
consuelita <adelita@operatorfoundation.org>
2021-01-05 14:35:58 -0600

    Update Bootstrap.swift

--- Sources/Gardener/Bootstrap.swift
+++ Sources/Gardener/Bootstrap.swift
@@ -78,6 +78,7 @@ public struct SwiftVersion
                     "libsqlite3-0",
                     "libstdc++-9-dev",
                     "libxml2",
+                    "libz3",
                     "libz3-dev",
                     "pkg-config",
                     "tzdata",
