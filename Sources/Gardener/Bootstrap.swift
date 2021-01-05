d6529dd5bb9853976cf813fc42565a55d4cf2596
consuelita <adelita@operatorfoundation.org>
2021-01-05 14:31:31 -0600

    Update Bootstrap.swift

--- Sources/Gardener/Bootstrap.swift
+++ Sources/Gardener/Bootstrap.swift
@@ -67,7 +67,6 @@ public struct SwiftVersion
                 ]
             case "20.04":
                 self.dependencies = [
-                    "install",
                     "binutils",
                     "git",
                     "gnupg2",
