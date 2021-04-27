8e28488c99629fb8bac6324ef792b94d8f79f6b5
consuelita <adelita@operatorfoundation.org>
2021-04-27 15:50:05 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "Gardener",
-    platforms: [.macOS(.v10_15)],
+    platforms: [.macOS(.v11)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
@@ -15,7 +15,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/OperatorFoundation/Song", from: "0.1.3"),
+        .package(url: "https://github.com/OperatorFoundation/Song", from: "0.1.4"),
         .package(url: "https://github.com/OperatorFoundation/Datable", from: "3.0.3"),
         .package(url: "https://github.com/OperatorFoundation/Chord", from: "0.0.6"),
     ],
