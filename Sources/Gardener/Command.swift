eafb098abb353c9f21a710aa66f477e94826cd01
CryptoSax <joshua@operatorfoundation.org>
2021-08-02 13:33:18 -0500

    added cancellable run command

--- Sources/Gardener/Command.swift
+++ Sources/Gardener/Command.swift
@@ -132,4 +132,78 @@ public class Command
 
         return (exitCode, resultData, errData)
     }
+    
+    public func runWithCancellation(_ command: String, _ args: String...) -> Cancellable?
+    {
+        guard command.count > 0
+        else
+        {
+            print("Run command failed. We couldn't understand the command \(command)")
+            return nil
+        }
+
+        var absolutePath = command
+
+        var pathFound = false
+        if command.first! != "/"
+        {
+            for attempt in path
+            {
+                absolutePath = attempt + "/" + command
+                if FileManager.default.fileExists(atPath: absolutePath)
+                {
+                    pathFound = true
+                    break
+                }
+            }
+        }
+        else
+        {
+            pathFound = true
+        }
+
+        guard pathFound
+        else
+        {
+            print("\nRun command failed. Path not found.")
+            print("Path: \(path)")
+            print("Command: \(command)")
+            return nil
+        }
+        
+        let stdoutPipe = Pipe()
+        let stderrPipe = Pipe()
+        let process = Process.init()
+        process.executableURL = URL(fileURLWithPath: absolutePath)
+        process.arguments = args
+        process.standardOutput = stdoutPipe
+        process.standardError = stderrPipe
+        process.currentDirectoryURL = URL(fileURLWithPath: FileManager.default.currentDirectoryPath)
+
+        do
+        {
+            try process.run()
+        }
+        catch
+        {
+            return nil
+        }
+
+        return Cancellable(process: process)
+    }
+}
+
+public struct Cancellable {
+    let process: Process
+    
+    public init(process: Process) {
+        self.process = process
+    }
+    
+    public func cancel() {
+        self.process.interrupt()
+        if process.isRunning {
+            process.terminate()
+        }
+    }
 }
