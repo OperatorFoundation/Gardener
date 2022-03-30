58dbc8e13b38f237acb0bc7041211813d2bbafd4
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2022-03-30 18:58:54 -0500

    Fixed Song dependency

--- Sources/Gardener/Gardener.swift
+++ Sources/Gardener/Gardener.swift
@@ -1,50 +1,14 @@
 import Foundation
-import Song
 
 public class Gardener: Codable
 {
-    static public var instance: Gardener = Gardener.load()
-    
-    static public func load() -> Gardener
-    {
-        let filename = "config.song"
-        let url = URL(fileURLWithPath: filename)
-        let song = SongDecoder()
-        
-        do
-        {
-            let data = try Data(contentsOf: url)
-            let result = try song.decode(Gardener.self, from: data)
-            return result
-        }
-        catch
-        {
-            return Gardener()
-        }
-    }
-    
+    static public let instance = Gardener()
+
     public var swiftPath: String?
     {
         didSet
         {
             Command.swiftPath = swiftPath
-        }
-    }
-    
-    public func save()
-    {
-        let filename = "config.song"
-        let url = URL(fileURLWithPath: filename)
-        let song = SongEncoder()
-        
-        do
-        {
-            let data = try song.encode(self)
-            try data.write(to: url)
-        }
-        catch
-        {
-            return
         }
     }
 }
