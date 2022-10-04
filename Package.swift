421ab351f9f252b0aeb9da6e227ce0f08e701856
Unseelie <emunseelie@gmail.com>
2022-10-04 12:37:01 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,7 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "Gardener",
-    platforms: [.macOS(.v12)],
+    platforms: [.macOS(.v12),
+                .iOS(.v15)
+               ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
