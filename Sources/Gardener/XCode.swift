c2d5afa8ebe1be8896c8b01332e4fbd06644c332
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2023-11-13 15:01:58 -0600

    xcode commands

--- Sources/Gardener/XCode.swift
+++ Sources/Gardener/XCode.swift
@@ -25,5 +25,40 @@ public class XCode
                             "--username", username,
                             "--password", password)
     }
+
+    func build(path: String) throws -> (exitCode: Int32, resultData: Data, errorData: Data)?
+    {
+        guard File.pushd(path) else
+        {
+            throw XCodeError.cdFailed(path)
+        }
+
+        let result = self.command.run("xcodebuild")
+
+        let _ = File.popd()
+
+        return result
+    }
+
+    func run(path: String) throws
+    {
+        let url = URL(fileURLWithPath: path)
+        let projectName = url.lastPathComponent
+
+        guard File.pushd(path) else
+        {
+            throw XCodeError.cdFailed(path)
+        }
+
+        let _ = self.command.run("build/Release/\(projectName).app")
+
+        let _ = File.popd()
+    }
 }
+
+public enum XCodeError: Error
+{
+    case cdFailed(String)
+}
+
 #endif
