c52495e068dec7b8795c90fc57e1ece07dc18a76
bragelbytes <joebragel@gmail.com>
2023-12-14 17:13:52 -0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -19,10 +19,10 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.3"),
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.2.3"),
         .package(url: "https://github.com/apple/swift-system", from: "1.2.1"),
 
-        .package(url: "https://github.com/OperatorFoundation/Chord.git", branch: "main"),
+        .package(url: "https://github.com/OperatorFoundation/Chord", branch: "main"),
         .package(url: "https://github.com/OperatorFoundation/Datable", branch: "main"),
         .package(url: "https://github.com/Bouke/Glob", from: "1.0.5"),
         .package(url: "https://github.com/nerdishbynature/octokit.swift", from: "0.12.0"),
