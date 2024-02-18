69cba9b544d1627eecffa60853ae39e20dfb6be2
consuelita <adelita@operatorfoundation.org>
2024-02-17 19:46:42 -0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -21,11 +21,12 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.2.3"),
         .package(url: "https://github.com/apple/swift-system", from: "1.2.1"),
-
-        .package(url: "https://github.com/OperatorFoundation/Chord", branch: "0.1.1"),
-        .package(url: "https://github.com/OperatorFoundation/Datable", branch: "4.0.0"),
+        
         .package(url: "https://github.com/Bouke/Glob", from: "1.0.5"),
         .package(url: "https://github.com/nerdishbynature/octokit.swift", from: "0.12.0"),
+        
+        .package(url: "https://github.com/OperatorFoundation/Chord", from: "0.1.4"),
+        .package(url: "https://github.com/OperatorFoundation/Datable", from: "4.0.1"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
